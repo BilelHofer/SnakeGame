6f8299fb0f124a7601936fe8bd785fbe5922f591
Bilel Hofer <bilel.hofer@ik.me>
2023-02-01 13:11:27 +0100

    Ajout fonctionnement du bouton quitter

--- app/src/main/java/com/example/snakegame/HomeFragment.java
+++ app/src/main/java/com/example/snakegame/HomeFragment.java
@@ -7,10 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
     public HomeFragment() {}
+
+    private Button ButtonPlay;
+    private Button ButtonScore;
+    private Button ButtonExit;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,33 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Initialise les boutons
+        ButtonPlay = view.findViewById(R.id.btn_play);
+        ButtonScore = view.findViewById(R.id.btn_score);
+        ButtonExit = view.findViewById(R.id.btn_exit);
+
+        // Ajoute les listeners
+        ButtonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+        ButtonScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+        ButtonExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().finish();
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/snakegame/MainActivity.java
+++ app/src/main/java/com/example/snakegame/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.snakegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Ajoute le fragment HomeFragment
         getSupportFragmentManager().beginTransaction().add(R.id.container, new HomeFragment()).commit();
     }
 }
