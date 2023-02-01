ac8d5aadda785443a6ad3a353c4b74889e563a55
Bilel Hofer <bilel.hofer@ik.me>
2023-02-01 15:24:25 +0100

    Ajout du fonctionnement de la database

---
+++ app/src/main/java/com/example/snakegame/DatabaseHelper.java
@@ -0,0 +1,71 @@
+package com.example.snakegame;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "snakegame.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "scores";
+    private static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_APPLES = "apples";
+
+    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_NAME + " TEXT NOT NULL, "
+            + COLUMN_APPLES + " INTEGER NOT NULL);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    /**
+     * Ajoute une score dans la base de données
+     * @param name Nom du joueur
+     * @param apples Nombre de pomme mangées
+     */
+    public void addScore(String name, int apples) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_APPLES, apples);
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    /**
+     * Retourne un curseur contenant tous les scores triés par ordre décroissant des pommes mangées
+     * @return Cursor contenant tous les scores
+     */
+    public Cursor getAllScores() {
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME +
+                " ORDER BY " + COLUMN_APPLES + " DESC";
+        return db.rawQuery(query, null);
+    }
+
+    /**
+     * Supprime la base de données et la recrée
+     */
+    public void resetDatabase() {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+}
--- app/src/main/java/com/example/snakegame/ScoreFragment.java
+++ app/src/main/java/com/example/snakegame/ScoreFragment.java
@@ -1,6 +1,8 @@
 package com.example.snakegame;
 
 import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,17 +10,83 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import java.util.Random;
 
 public class ScoreFragment extends Fragment {
 
+    private ListView mScoresListView;
+    private SimpleCursorAdapter mAdapter;
+    //TODO button temporaire
+    private Button ButtonAdd;
+    private DatabaseHelper dbHelper;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        dbHelper = new DatabaseHelper(getContext());
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
+        View view = inflater.inflate(R.layout.fragment_score, container, false);
+
+        ButtonAdd = view.findViewById(R.id.btn_add);
+
+        ButtonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //dbHelper.resetDatabase(); // reset de la db
+
+                // Ajout de données
+                Random rn = new Random();
+                dbHelper.addScore("TEST", rn.nextInt(10) + 1);
+
+                // Chargez les données depuis la base de données
+                loadScores();
+            }
+        });
+
+        mScoresListView = view.findViewById(R.id.list_scores);
+
+        // Spécifiez les colonnes à afficher dans la liste
+        String[] columns = { DatabaseHelper.COLUMN_NAME, DatabaseHelper.COLUMN_APPLES};
+
+        // Spécifiez les vues dans lesquelles les données doivent être affichées
+        int[] viewIds = { android.R.id.text1, android.R.id.text2 };
+
+        mAdapter = new SimpleCursorAdapter(getContext(), android.R.layout.simple_list_item_2, null, columns, viewIds, 0);
+        mScoresListView.setAdapter(mAdapter);
+
+        // Chargez les données depuis la base de données
+        loadScores();
+
+        return view;
+    }
+
+    /**
+     * Charge les scores depuis la base de données
+     */
+    private void loadScores() {
+        new LoadScoresTask().execute();
+    }
+
+    /**
+     * Tâche asynchrone pour charger les scores depuis la base de données
+     */
+    private class LoadScoresTask extends AsyncTask<Void, Void, Cursor> {
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            return dbHelper.getAllScores();
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            mAdapter.swapCursor(cursor);
+        }
     }
 }
