6dc5b30d06633259da2bb257450bc7fca58f4579
Bilel Hofer <bilel.hofer@ik.me>
2023-02-02 10:22:17 +0100

    Ajout d'une image à l'application

--- app/src/main/java/com/example/snakegame/GameFragment.java
+++ app/src/main/java/com/example/snakegame/GameFragment.java
@@ -1,13 +1,21 @@
 package com.example.snakegame;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -25,5 +33,46 @@ public class GameFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        // TODO faire que le builder ne se ferme pas si le nom est trop long
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        // Set the title for the dialog
+        builder.setTitle("Enter text");
+
+        // Create an EditText widget
+        final EditText input = new EditText(requireActivity());
+
+        // Set the EditText as the view for the dialog
+        builder.setView(input);
+
+        // Add positive button
+        builder.setPositiveButton("Sauvez", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Get the input text
+                String inputText = input.getText().toString();
+
+                if (inputText.length() > 12) {
+                    Toast.makeText(requireActivity(), "Le nom ne doit pas dépasser 12 caractères", Toast.LENGTH_SHORT).show();
+                } else {
+                    // TODO l'ajouter dans la db
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.setNegativeButton("Abandonner", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Close the dialog
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 }
